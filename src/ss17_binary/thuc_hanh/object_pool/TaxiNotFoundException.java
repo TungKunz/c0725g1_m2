e732aaeb4bf84df2612464eaf2a3fba95b0d339b
TungKunz <viettung0203@gmail.com>
2025-10-06 16:35:04 +0700

    ss17

--- src/ss17_binary/thuc_hanh/object_pool/TaxiNotFoundException.java
+++ src/ss17_binary/thuc_hanh/object_pool/TaxiNotFoundException.java
@@ -1,4 +1,8 @@
 package ss17_binary.thuc_hanh.object_pool;
 
-public class TaxiNotFoundException {
+public class TaxiNotFoundException extends RuntimeException {
+
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
 }
