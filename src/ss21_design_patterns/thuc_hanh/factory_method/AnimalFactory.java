131015d16126f0304398481edfd834f98a25cb5a
TungKunz <viettung0203@gmail.com>
2025-10-06 16:35:33 +0700

    ss21

--- src/ss21_design_patterns/thuc_hanh/factory_method/AnimalFactory.java
+++ src/ss21_design_patterns/thuc_hanh/factory_method/AnimalFactory.java
@@ -1,4 +1,11 @@
 package ss21_design_patterns.thuc_hanh.factory_method;
 
-public class AnimalFactorty {
+public class AnimalFactory {
+    public Animal getAnimal(String type) {
+        if ("canine".equals(type)) {
+            return new Dog();
+        } else {
+            return new Cat();
+        }
+    }
 }
--- src/ss21_design_patterns/thuc_hanh/factory_method/Dog.java
+++ src/ss21_design_patterns/thuc_hanh/factory_method/Dog.java
@@ -1,4 +1,8 @@
 package ss21_design_patterns.thuc_hanh.factory_method;
 
-public class Dog {
+public class Dog extends Animal {
+    @Override
+    public String makeSound() {
+        return "Woof";
+    }
 }
